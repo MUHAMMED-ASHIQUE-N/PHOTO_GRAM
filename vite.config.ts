6dd70f66590a299423bcea11c0cc7a395ebcbb7d
MUHAMMED-ASHIQUE-N <muhammedashique433@gmail.com>
2025-06-26 20:41:23 +0530

    create PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,54 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import {VitePWA, type VitePWAOptions} from "vite-plugin-pwa"
+
+
+
+const manifestForPlugIn:  Partial<VitePWAOptions> = {
+  registerType:'prompt',
+  includeAssets:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+} as const
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA(manifestForPlugIn)],
     server: {
     port: 3000,
     open: true,
@@ -17,3 +61,7 @@ export default defineConfig({
     },
   },
 })
+
+
+
+
